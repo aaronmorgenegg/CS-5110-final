c84274486b2640a0887db007d1aee2b0aed531ad
Aaron <aaron.morgenegg@verizondigitalmedia.com>
2018-04-24 13:43:33 -0600

    Started adding connections graph

--- code/simulation.py
+++ code/simulation.py
@@ -8,6 +8,7 @@ import random
 
 RATIO_AD = .9 # Ratio of agents that start as Always Defect
 NUMBER_OF_GAMES = 10 # number of games agents will play for each round
+REWIRING_PROBABILITY = .2 # chance for agents to be connected
 
 
 class Agent:
@@ -16,6 +17,8 @@ class Agent:
         self.trust = self.GetStartingTrust()
         self.payoff = 0
         self.last_result = True
+        self.discontent = 0
+        self.content = 0
 
     def Play(self):
         if self.trust is False:
@@ -33,6 +36,9 @@ class Agent:
 
     def ResetPayoff(self):
         self.payoff = 0
+
+    def __str__(self):
+        return('Trust({}) Payoff({}) Discontent({}) Content({})'.format(self.trust, self.payoff, self.discontent, self.content))
 
 def PrisonersDilemma(agentA, agentB):
     if agentA.Play() is True and agentB.Play() is True:
@@ -67,6 +73,7 @@ def IteratedPrisonersDilemma(agentA, agentB):
 class Society:
     def __init__(self, num_agents):
         self.agents = self.GetStartingAgents(num_agents)
+        self.connections = self.SetConnectedAgents()
 
     def GetStartingAgents(self, num_agents):
         agents = []
@@ -74,21 +81,46 @@ class Society:
             agents.append(Agent())
         return agents
 
+    def SetConnectedAgents(self):
+        connections = []
+        for i in range(len(self.agents)):
+            connections.append([])
+
+        for i in range(len(self.agents)-1):
+            for j in range(i+1, len(self.agents)):
+                if(random.random() <= REWIRING_PROBABILITY):
+                    if j not in connections[i]: connections[i].append(j)
+                    if i not in connections[j]: connections[j].append(i)
+
+        return connections
+
     def RunIteratedPrisonersDilemma(self):
         self.ResetPayoffs()
         for i in range(len(self.agents)-1):
             for j in range(i+1, len(self.agents)):
                 IteratedPrisonersDilemma(self.agents[i], self.agents[j])
 
+        self.UpdateStrategies()
 
     def ResetPayoffs(self):
         for agent in self.agents:
             agent.ResetPayoff()
 
+    def MeasureTrust(self):
+        count = 0
+        for agent in self.agents:
+            if agent.trust:
+                count += 1
+
+        return count
+
+    def UpdateStrategies(self):
+        pass
+
     def __str__(self):
         society_str = '--- Society ---\n'
         for i in range(len(self.agents)):
-            society_str += 'Agent: {}, Trust: {}, Payoff: {}\n'.format(i, self.agents[i].trust, self.agents[i].payoff)
+            society_str += 'Agent({}) {}\n'.format(i, str(self.agents[i]))
 
         return society_str
 
